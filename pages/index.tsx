2651b8e8b55879dc60ef6811e44dad586ce6d8dc
davirsreis <daviresende.2312@gmail.com>
2023-10-10 10:33:26 -0300

    Criando componente Enunciado

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import Questao from '../components/Questao'
+import QuestaoModel from '../model/questao'
+import RespostaModel from '../model/resposta'
+
+export default function Home() {
+
+  const questaoTeste = new QuestaoModel(1, "Melhor cor?", [
+    RespostaModel.errada("Verde"),
+    RespostaModel.errada("Vermelha"),
+    RespostaModel.errada("Preta"),
+    RespostaModel.certa("Azul"),
+  ])
+
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}>
+      <Questao valor={questaoTeste} />
+
+    </div>
+  )
+}
