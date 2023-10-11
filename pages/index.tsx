eb11165b10f2a2e27f32738dc10fecfd0073b5b8
davirsreis <daviresende.2312@gmail.com>
2023-10-11 16:28:43 -0300

    Implementando Index e tela de resultado

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,10 +40,8 @@ export default function Home() {
   }
 
   function idProximaPergunta() {
-    if (questao) {
-      const proximoIndice = idsDasQuestoes.indexOf(questao.id) + 1
-      return idsDasQuestoes[proximoIndice]
-    }
+    const proximoIndice = idsDasQuestoes.indexOf(questao.id) + 1
+    return idsDasQuestoes[proximoIndice]
   }
 
   function irParaProximoPasso() {
@@ -65,12 +63,11 @@ export default function Home() {
     })
   }
 
-  return (
+  return questao ?
     <Questionario
       questao={questao}
       ultima={idProximaPergunta() === undefined}
       questaoRespondida={questaoRespondida}
       irParaProximoPasso={irParaProximoPasso}
-    />
-  )
+    /> : false
 }
